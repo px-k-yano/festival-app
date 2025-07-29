003a5caffd5bf2232d9b1160458ec411faa56674
Ubuntu <azureuser@festival-app-dev-vm.5j4ixsllzm4uhkjop3nqodeiqh.lx.internal.cloudapp.net>
2025-07-29 06:44:33 +0000

    テーブル構成を修正

--- api/db/init.sql
+++ api/db/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-  blockchain_account_address VARCHAR(34) PRIMARY KEY,
+  blockchain_account_address VARCHAR(50) PRIMARY KEY,
   nickname VARCHAR(50) NOT NULL,
   token_id VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -8,10 +8,13 @@ CREATE TABLE users (
 
 CREATE TABLE photos (
   id SERIAL PRIMARY KEY,
-  hash_value VARCHAR(255) NOT NULL DEFAULT '',
-  photo_url TEXT NOT NULL DEFAULT '',
-  likes INTEGER NOT NULL DEFAULT 0,
+  hash_value VARCHAR(64) NOT NULL DEFAULT '',
+  instagram_photo_url VARCHAR(255) NOT NULL DEFAULT '',
   upload_status VARCHAR(20) NOT NULL DEFAULT 'Boxアップロード待ち',
+  token_id VARCHAR(255) NOT NULL DEFAULT '',
+  likes INTEGER NOT NULL DEFAULT 0,
+  blockchain_account_address VARCHAR(50) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (blockchain_account_address) REFERENCES users(blockchain_account_address)
 );
